37eba15107bd97d068d58b06a0ed1633092d72d7
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2025-11-14 10:51:59 -0300

    FEAT: Font added. #2

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
 <template>
     <header>
         <div>
-            <nav class="pt-3 ml-5 flex gap-15 text-lg text-[#f6a233]">
+            <nav class="pt-3 ml-5 flex gap-15 text-xl text-[#f6a233]">
                 <router-link to="/" class="w-10"><img src="/images/golden_era.svg" alt="logo GoldenEra"></router-link>
-                <router-link to="/" class="hover:text-white font-[Girassol]">Home</router-link>
-                <router-link to="/filmes" class="hover:text-white">Filmes</router-link>
+                <router-link to="/" class="hover:text-white font-[Girassol] tracking-widest">Home</router-link>
+                <router-link to="/filmes" class="hover:text-white font-[Girassol] tracking-widest">Filmes</router-link>
             </nav>
         </div>
 
